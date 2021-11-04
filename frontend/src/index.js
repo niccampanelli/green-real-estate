283fe0039740009a4c60d095c8b62fa22ffd99a8
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-11-03 22:43:27 -0300

    Removendo imports desnecessários

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.css';
-import App from './App';
 import Routes from './routes'
 import Header from './View/Header'
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
