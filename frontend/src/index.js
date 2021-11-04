084daf53ddd7fee3c559765a7aa10aa8ef01b89d
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-11-03 22:34:14 -0300

    Criando header

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './global.css';
 import App from './App';
+import Routes from './routes'
+import Header from './View/Header'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    <Routes />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
