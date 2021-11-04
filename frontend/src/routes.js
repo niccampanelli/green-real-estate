ac5089a5e26b54cee2b11438d646b885402df4e9
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-11-03 22:36:04 -0300

    Incluindo componente home na rota

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './View/Home/';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path='/' />
+                <Route path='/' component={Home}/>
             </Switch>
         </BrowserRouter>
     );
