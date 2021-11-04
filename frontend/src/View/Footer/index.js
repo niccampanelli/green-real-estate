4b8715645e03b5e7d1e194cd441ea66380ca3b60
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-11-04 02:06:39 -0300

    Footer

--- frontend/src/View/Footer/index.js
+++ frontend/src/View/Footer/index.js
@@ -1,16 +1,34 @@
 import React from "react";
+import "./style.css";
 
 export default function Footer() {
     return(
         <footer>
-            <nav>
-                <h2>Páginas</h2>
-                <ul>
-                    <li>Início</li>
-                    <li>Buscar Imóveis</li>
-                    <li>Sobre Nós</li>
-                </ul>
+            <nav className="footerNav">
+                <div className="footerNavColumn">
+                    <h2>Páginas</h2>
+                    <ul>
+                        <li>Início</li>
+                        <li>Buscar Imóveis</li>
+                        <li>Sobre Nós</li>
+                    </ul>
+                </div>
+                <div className="footerNavColumn">
+                    <h2>Endereço</h2>
+                    <ul>
+                        <li>Rua Patativa, 176 - Cidade Antônio Estêvão de Carvalho, São Paulo - SP, 08220-010</li>
+                    </ul>
+                </div>
+                <div className="footerNavColumn">
+                    <h2>Redes Sociais</h2>
+                    <ul>
+                        <li><a href="https://www.facebook.com/gleusaimoveis/">Facebook</a></li>
+                        <li><a href="https://www.instagram.com/gleusaimoveis/">Instagram</a></li>
+                    </ul>
+                </div>
             </nav>
+            <p>© 2021 Gleusa Imóveis - CRECI: 25395-J | CNPJ: 09.008.578/0001-69</p>
+            <p>Desenvolvido por <a href="https://mkstudio.herokuapp.com/"><img alt="MK STUDIO"/></a></p>
         </footer>
     );
 }
