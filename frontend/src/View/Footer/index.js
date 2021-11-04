4430857b51a401657b0af8c25a5285efabd86a34
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-11-03 23:03:20 -0300

    Iniciando Footer

---
+++ frontend/src/View/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Footer() {
+    return(
+        <>
+        <footer>
+            <nav>
+                <h2>Páginas</h2>
+                <ul>
+                    <li>Início</li>
+                    <li>Buscar Imóveis</li>
+                    <li>Sobre Nós</li>
+                </ul>
+            </nav>
+        </footer>
+        </>
+    );
+}
