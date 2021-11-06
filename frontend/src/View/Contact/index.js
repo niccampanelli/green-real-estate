8d2cc4aff012d34c312421919df13e435d2e78d0
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-11-05 23:48:07 -0300

    Criação do componente sobre

---
+++ frontend/src/View/Contact/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import './style.css';
+
+export default function Contact() {
+    return(
+        <main>
+            <article>
+                <section className="presentationSection">
+                    <h1>Sobre nós</h1>
+                </section>
+                <section className="historySection">
+                    <h2>Nossa história</h2>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                </section>
+                <section>
+                    <h2>Nosso propósito</h2>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                </section>
+                <section>
+                    <h2>Opiniões dos clientes</h2>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                </section>
+                <section>
+                    <h2>Converse conosco</h2>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    <br/>
+                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                </section>
+            </article>
+        </main>
+    );
+}
