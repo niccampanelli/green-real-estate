9a9e11b4f7a59433f677741a7fdb671174b9267b
Lucas Campanelli <lucas.campanelli@outlook.com>
2021-11-06 02:45:14 -0300

    Sobre nós

--- frontend/src/View/Contact/index.js
+++ frontend/src/View/Contact/index.js
@@ -16,60 +16,45 @@ export default function Contact() {
                     </div>
                 </section>
                 <section className="historySection">
-                    <h2>Nossa história</h2>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
+                    <h2 className="historySubtitle">Nossa história</h2>
                     <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
                     <br/>
                     <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
                     <br/>
                     <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
                 </section>
-                <section>
-                    <h2>Nosso propósito</h2>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                <section className="purposeSection">
+                    <div className="purposeLeft">
+                        <img alt="Ilustração"/>
+                    </div>
+                    <div className="purposeRight">
+                        <h2 className="purposeSubtitle">Nosso propósito</h2>
+                        <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                        <br/>
+                        <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                    </div>
                 </section>
-                <section>
-                    <h2>Opiniões dos clientes</h2>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                <section className="opinionSection">
+                    <div className="opinionSubtitleContainer">
+                        <h2 className="opinionSubtitle">Opiniões dos clientes</h2>
+                    </div>
+                    <p className="opinionDescription">Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint.</p>
+                    
                 </section>
-                <section>
-                    <h2>Converse conosco</h2>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
-                    <br/>
-                    <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                <section className="contactSection">
+                    <div className="contactLeft">
+                        <h2 className="contactSubtitle">Converse conosco</h2>
+                        <p>Eiusmod irure cupidatat quis est ipsum in id. Cupidatat non proident tempor velit culpa est anim labore dolore sunt ea cillum cillum eu. Reprehenderit deserunt reprehenderit dolore ad velit duis aliquip enim elit dolor. Officia laborum cupidatat reprehenderit velit magna aliquip consequat sint. Dolor pariatur officia nisi aliqua mollit enim sit anim. Dolor nulla sit mollit eu consequat duis eu amet ut labore sint elit. Amet esse laborum in non aute commodo ex irure excepteur.</p>
+                        <div className="contactContainer">
+                            <ul className="contactList">
+                                <li className="contactItem">(11) 1111-1111</li>
+                                <li className="contactItem">nononon@mail.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="contactRight">
+                        <img alt="Ilustração" />
+                    </div>
                 </section>
             </article>
         </main>
