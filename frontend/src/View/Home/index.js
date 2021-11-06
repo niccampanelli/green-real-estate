093fc5376b3534399d85364f0b9b0fa635590b4a
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-11-05 23:47:29 -0300

    Colo rule

--- frontend/src/View/Home/index.js
+++ frontend/src/View/Home/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./style.css";
 
 export default function Home() {
     return(
-        <div></div>
+        <main>
+            <section className="landing">
+                <form className="landingSearch">
+                    <input className="searchInput"/>
+                    <button type="submit" className="searchInputButton">enviar</button>
+                </form>
+            </section>
+        </main>
     );
 }
