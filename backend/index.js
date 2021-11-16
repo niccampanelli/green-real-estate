e0b13e242c7e8ac7bdbc4278c43a6a61beab42c5
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-11-16 20:42:41 -0300

    Resolving ENV

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
 const routes = require('./routes');
 const app = express();
 const port = 3333;
-
-require('dotenv').config({
-    path: path.resolve(__dirname, ".env")
-});
 
 app.use(cors());
 app.use(routes);
--- backend/src/Database/Connection.js
+++ backend/src/Database/Connection.js
@@ -1,4 +1,10 @@
 const mariadb = require('mariadb');
+const path = require('path');
+
+require('dotenv').config({
+    path: path.resolve(__dirname, "../../.env")
+});
+
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
