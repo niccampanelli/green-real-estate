c3b3fd0d2cbc5ef30b49eed7609b070f6966b7ad
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2021-11-16 20:36:49 -0300

    Removing ConsoleLog

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const app = express();
-const routes = require('./routes');
 const cors = require('cors');
 const path = require('path');
+const routes = require('./routes');
+const app = express();
 const port = 3333;
-const conn = require('./src/Database/Connection');
+
 require('dotenv').config({
     path: path.resolve(__dirname, ".env")
 });
 
 app.use(cors());
-app.listen(port);
 app.use(routes);
-
-conn.initializeDatabase();
+app.listen(port);
